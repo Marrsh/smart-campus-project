14b4c5945ee1f051420dc61da8ac4f1c84b9ab21
unknown <chrism4rsh@gmail.com>
2021-12-20 20:33:12 +0000

    add bottom nav

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
+
+const String SUCCESS_MESSAGE=" You will be contacted by us very soon.";
+
+var physicalScreenSize = window.physicalSize;
+var pixelRatio = window.devicePixelRatio;
+var logicalScreenSize = window.physicalSize / pixelRatio;
+
+var height10 = logicalScreenSize.height * 0.1;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,12 +8,68 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("height is $logicalScreenSize");
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.amber), // declare theme data for this widget?
-      home: Scaffold( //Like stacks in swift, takes in the elements of the widget (body, appbar etc..)
+      home: Scaffold(//Like stacks in swift, takes in the elements of the widget (body, appbar etc..)
         appBar: AppBar(title: Center(child: Text('University Name'))),
-        body: Center(child: Text('Hello World'))
-      )
+      body: _NavBar(),
+        )
+      );
+
+  }
+}
+
+class _NavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children: [
+          Positioned(
+              bottom: 0,
+              left: 0,
+              child: Container(
+                width: logicalScreenSize.width,
+                height: height10,
+                color: Colors.blue,
+                child: Row(
+                  children: [
+                    _NavButton(),
+                    _NavButton(),
+                    _CentralNavButton(),
+                    _NavButton(),
+                    _NavButton()
+                  ],
+                ),
+              )
+          ),
+        ],
+    );
+  }
+}
+
+class _NavButton extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Container(
+            width: height10,
+            height: height10,
+            color: Colors.amber,
+            margin: const EdgeInsets.only(left:3),
+    );
+  }
+}
+
+class _CentralNavButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.yellow,
+      shape: CircleBorder(),
+      onPressed: () {},
+        child: Text(
+          'A circle button',
+          style: TextStyle(color: Colors.white, fontSize: 5),
+      ),
     );
   }
 }
